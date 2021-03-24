24dba75bd9a53ccb39815245f051f72096f48a46
supminn <30731236+supminn@users.noreply.github.com>
2021-03-24 17:38:46 +0530

    feat: radio and checkbox

--- scripts/docs.js
+++ scripts/docs.js
@@ -1,7 +1,14 @@
 const btnNavMain = document.querySelector('#btn-navigation');
 const navListMain = document.querySelector('#list-nav-main');
+const linkItem = document.querySelectorAll('.btn-link');
 
 btnNavMain.addEventListener('click',() => {
     navListMain.classList.toggle('nav-list-hide');
     navListMain.classList.toggle('list-inline');
 })
+
+for(item of linkItem){
+    item.addEventListener('click',() =>{
+        navListMain.classList.toggle('nav-list-hide');
+    })
+}
